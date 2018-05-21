9a4a7eab9b185524798f742ee8012cc97b39db00
steven garner <stevengarner3@gmail.com>
2018-05-21 19:15:37 +0100

    first setup of app

---
+++ scabless.py
@@ -0,0 +1,12 @@
+#first set up function that holds the words.
+# #import random module
+# print words then end in "ing", "er" "ies"
+#set up a dictionary key pair.
+
+import random
+
+
+def holder():
+    guess = input("")
+    print("hello" + guess)
+holder()
